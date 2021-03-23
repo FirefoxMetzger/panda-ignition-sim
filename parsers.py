02594fad46da88d86b97ecdffa80acd92a412b4f
FirefoxMetzger <sebastian@wallkoetter.net>
2021-03-23 17:04:07 +0100

    remove files that moved to ropy

--- parsers.py
+++ parsers.py
@@ -2,9 +2,7 @@
 A collection of parsers used to deserialize messages used in the simulator
 """
 
-from ignition.msgs.image_pb2 import Image
-from ignition.msgs.clock_pb2 import Clock
-from ignition.msgs.camera_info_pb2 import CameraInfo
+from ropy.ignition.msgs import Image, Clock, CameraInfo
 from dataclasses import dataclass
 import numpy as np
 
@@ -15,13 +13,13 @@ class ImageMessage:
 
 def clock_parser(msg):
     clock_msg = Clock()
-    clock_msg.ParseFromString(msg[2])
+    clock_msg.parse(msg[2])
     sim_time = clock_msg.sim.sec + clock_msg.sim.nsec*1e-9
     return sim_time
 
 def camera_parser(msg):
     image_msg = Image()
-    image_msg.ParseFromString(msg[2])
+    image_msg.parse(msg[2])
 
     im = np.frombuffer(image_msg.data, dtype=np.uint8)
     im = im.reshape((image_msg.height, image_msg.width, 3))
@@ -33,6 +31,6 @@ def camera_parser(msg):
 
 def camera_info_parser(msg):
     decoded_msg = CameraInfo()
-    decoded_msg.ParseFromString(msg[2])
+    decoded_msg.parse(msg[2])
 
     return decoded_msg
