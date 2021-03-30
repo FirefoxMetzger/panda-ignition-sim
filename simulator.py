4888f002ec424c879cc782c9cde2f8451d77937d
FirefoxMetzger <sebastian@wallkoetter.net>
2021-03-30 08:38:22 +0200

    spawn cubes on table

--- simulator.py
+++ simulator.py
@@ -3,6 +3,7 @@ from scenario import core as scenario_core
 import gym_ignition_environments
 from gym_ignition.rbd import conversions
 from gym_ignition.context.gazebo import controllers
+import gym_ignition
 
 
 import numpy as np
@@ -10,21 +11,23 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from scipy.spatial.transform import Rotation as R
 import ropy.ignition as ign
-import ropy.transform as tf
+import ropy.transform as rtf
 import ropy.trajectory as rtj
 from parsers import camera_parser, clock_parser
 
 from panda_controller import LinearJointSpacePlanner
+import imageio as iio
 
 import time
+import random
 
 gazebo = scenario_gazebo.GazeboSimulator(step_size=0.001, rtf=1.0, steps_per_run=1)
-assert gazebo.insert_world_from_sdf("./panda_world_expanded.sdf")
+assert gazebo.insert_world_from_sdf("./sdf/environment.sdf")
 gazebo.initialize()
+world = gazebo.get_world()
 
 # ---- initialize panda ----
 # spawn
-world = gazebo.get_world()
 panda = gym_ignition_environments.models.panda.Panda(
     world=world, position=[0.2, 0, 1.025]
 )
@@ -63,22 +66,74 @@ camera_frequency = 30  # Hz
 steps_per_frame = round((1 / gazebo.step_size()) / camera_frequency)
 
 # intrinsic matrix
-cam_intrinsic = tf.projections.camera_frustum(hfov=1.047, image_shape=(1080, 1920))
+cam_intrinsic = rtf.perspective_frustum(hfov=1.13446, image_shape=(1080, 1920))
 
 # extrinsic matrix
 cam_pos_world = np.array(camera.position())
 cam_ori_world_quat = np.array(camera.orientation())[[1, 2, 3, 0]]
 cam_ori_world = R.from_quat(cam_ori_world_quat).as_euler("xyz")
 camera_frame_world = np.stack((cam_pos_world, cam_ori_world)).ravel()
-cam_extrinsic = tf.coordinates.transform(camera_frame_world)
+cam_extrinsic = rtf.transform(camera_frame_world)
 # --- end initialize camera projection ---
 
 # --- begin define placeable area ---
-center = world.get_model("table2").get_link("link").position() + np.array((0, 0, 1.015))
+center = world.get_model("table2").get_link("link").position() + np.array(
+    (-0.13, 0, 1.015)
+)
+half_extent = np.array((0.25, 0.5, 0))
+
+with open("./sdf/cube_template.sdf.in", "r") as file:
+    cube_template = file.read()
+
+color_values = [
+    ".6 .75 .3 1",
+    ".25 .56 .99 1",
+    ".43 .84 .85 1",
+    ".98 .43 .27 1",
+    ".94 .82 .79 1",
+]
+
+
+def point_on_table():
+    return center + np.random.rand(3) * 2 * half_extent - half_extent
+
+
+def spawn_cube(position, velocity=np.array((0, 0, 0)), idx=0):
+    # Get a unique name
+    model_name = gym_ignition.utils.scenario.get_unique_model_name(
+        world=world, model_name="cube"
+    )
+
+    color = color_values[idx % len(color_values)]
+    model = cube_template.format(diffuse=color, ambient=color)
+
+    # Insert the model
+    assert world.insert_model(
+        model, scenario_core.Pose(position, [1.0, 0, 0, 0.274]), model_name
+    )
+
+    cube = world.get_model(model_name)
+    velocity = scenario_core.Array3d(velocity.tolist())
+    assert cube.to_gazebo().reset_base_world_linear_velocity(velocity)
+
+    return cube
+
+
 # --- end define placeable area ---
 
-
+time.sleep(3)
+writer = iio.get_writer("my_video.mp4", format="FFMPEG", mode="I", fps=30)
 fig, ax = plt.subplots(1)
+
+num_cubes = 6
+cube_positions = list()
+cubes = list()
+for idx in range(num_cubes):
+    pos = point_on_table() + np.array((0, 0, 0.025))
+    cubes.append(spawn_cube(pos, idx=idx))
+    cube_positions.append(pos)
+
+
 with ign.Subscriber("/clock", parser=clock_parser) as clock_topic, ign.Subscriber(
     "/camera", parser=camera_parser
 ) as camera_topic:
@@ -88,7 +143,8 @@ with ign.Subscriber("/clock", parser=clock_parser) as clock_topic, ign.Subscribe
     # of the sim loop instead of the beginning?
     gazebo.run(paused=True)
 
-    total_steps = 20000
+    num_targets = 3
+    total_steps = 2000 * num_targets
     for sim_step in range(total_steps):
         # get the current time (published each step)
         sim_time = clock_topic.recv()
@@ -96,38 +152,22 @@ with ign.Subscriber("/clock", parser=clock_parser) as clock_topic, ign.Subscribe
         # get synced camera images (published at 30Hz)
         if round(sim_time / gazebo.step_size()) % steps_per_frame == 0:
             img_msg = camera_topic.recv()
+            writer.append_data(img_msg.image)
 
             # get px coordinates of endeffector
-            eff_world = tf.homogenize(end_effector.position())
+            eff_world = rtf.homogenize(end_effector.position())
             eff_cam = np.matmul(cam_extrinsic, eff_world)
             eff_px_hom = np.matmul(cam_intrinsic, eff_cam)
-            eff_px = tf.cartesianize(eff_px_hom)
+            eff_px = rtf.cartesianize(eff_px_hom)
 
             ax.add_patch(Circle(eff_px, radius=6))
 
             # verify that image message and simulator are in sync
             assert sim_time == img_msg.time
 
-        if sim_step % 1000 == 0 and sim_step > 0:
-            # get target on screen
-            in_world = tf.homogenize(world_target)
-            in_cam = np.matmul(cam_extrinsic, in_world)
-            in_px_hom = np.matmul(cam_intrinsic, in_cam)
-            in_px = tf.cartesianize(in_px_hom)
-            ax.add_patch(Circle(in_px, radius=10, color="red"))
-
-            ax.imshow(img_msg.image)
-            ax.set_axis_off()
-            plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-            plt.margins(0, 0)
-            ax.xaxis.set_major_locator(plt.NullLocator())
-            ax.yaxis.set_major_locator(plt.NullLocator())
-            plt.show()
-            fig, ax = plt.subplots(1)
-
         if sim_step % 2000 == 0:
-            world_target = center + np.array((0, 0, 0.05))
-            joint_target = panda_ctrl.solve_ik(world_target)
+            world_target = random.choice(cube_positions) + np.array((0, 0, 0.03))
+
             t = np.arange(sim_step + 1, sim_step + 1001)
             move_to_goal = panda_ctrl.plan(
                 t,
@@ -145,13 +185,31 @@ with ign.Subscriber("/clock", parser=clock_parser) as clock_topic, ign.Subscribe
             full_trajectory = np.empty((2000, 9))
             full_trajectory[:1000] = move_to_goal
             full_trajectory[1000:] = move_from_goal
-            # full_trajectory[:, :] = move_to_goal[None, -1]
             plan_step = sim_step
+
+            # visualize target
+            in_world = rtf.homogenize(world_target)
+            in_cam = np.matmul(cam_extrinsic, in_world)
+            in_px_hom = np.matmul(cam_intrinsic, in_cam)
+            in_px = rtf.cartesianize(in_px_hom)
+            ax.add_patch(Circle(in_px, radius=10, color="red"))
 
         panda.set_joint_position_targets(
             full_trajectory[sim_step - plan_step], ik_joints
         )
 
         gazebo.run()
+
+writer.close()
+
+
+# visualize the trajectory
+ax.imshow(img_msg.image)
+ax.set_axis_off()
+plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+plt.margins(0, 0)
+ax.xaxis.set_major_locator(plt.NullLocator())
+ax.yaxis.set_major_locator(plt.NullLocator())
+plt.show()
 
 gazebo.close()
